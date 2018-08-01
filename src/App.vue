0582d9b35585d317b7c8fcd43a6cb7a087a19694
zhangxiang <zhangxiangzdx@163.com>
2018-08-01 08:22:37 +0800

    '静态页面'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-<div>
-  App
-</div>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFoot"/>
+  </div>
 </template>
 
 <script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+
   export default {
-    name: "app"
+    mounted() {
+      this.$store.dispatch('getAddress')
+    },
+    components: {
+      FooterGuide
+    }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+
 import App from './App'
+import router from './router/index'
+import store from './store'
 
 new Vue({
   el:'#app',
-  render: h => h(App)
+  render: h => h(App),
+  router,
+  store
 })
